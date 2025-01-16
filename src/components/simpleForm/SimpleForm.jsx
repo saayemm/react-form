c45539f53b2dc029f4cc9d16f481e3b4ac4269ec
Saayemm <sayemdcc@gmail.com>
2025-01-16 21:13:06 +0600

    first commit

---
+++ src/components/simpleForm/SimpleForm.jsx
@@ -0,0 +1,13 @@
+
+
+export default function SimpleForm() {
+  return (
+    <div>
+        <form>
+            <input type="text" />
+            <br />
+            <button>Submit</button>
+        </form>
+    </div>
+  )
+}
