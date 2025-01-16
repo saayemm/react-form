0d7fe1fe99452d2bb49b8cfe21b0ef809147e513
Saayemm <sayemdcc@gmail.com>
2025-01-16 23:02:16 +0600

    form value

--- src/components/simpleForm/SimpleForm.jsx
+++ src/components/simpleForm/SimpleForm.jsx
@@ -1,12 +1,40 @@
+import { useState } from "react";
 
 
 export default function SimpleForm() {
+
+    const [email, setEmail] = useState(null)
+    const [pass, setpass] = useState(null)
+    const [name, setname] = useState(null)
+
+    const hundleSubmit = e => {
+        e.preventDefault()
+        console.log(email);
+        console.log(pass);
+        console.log(name);
+    }
+
+    const hundleChangename = e => {
+        setname(e.target.value)
+    }
+
+    const hundleEmailChange = e => {
+        setEmail(e.target.value)
+    }
+
+    const hundleChangePass = e => {
+        setpass(e.target.value)
+    }
   return (
     <div>
-        <form>
-            <input type="text" />
+        <form onSubmit={hundleSubmit}>
+            <input onChange={hundleChangename} type="text" name="name" />
             <br />
-            <button>Submit</button>
+            <input onChange={hundleEmailChange} type="email" name="email" />
+            <br />
+            <input onChange={hundleChangePass} type="password" name="password" />
+            <br />
+            <input type="submit" value="Submit"/>
         </form>
     </div>
   )
