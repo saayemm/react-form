be1e568916ac8daba80b31fe5d3936fb87de3214
Saayemm <sayemdcc@gmail.com>
2025-01-17 14:16:04 +0600

    add context api

--- src/components/grandpa/Grandpa.jsx
+++ src/components/grandpa/Grandpa.jsx
@@ -1,17 +1,22 @@
+import { createContext } from "react"
 import Anty from "../anty/Anty"
 import Dad from "../dad/Dad"
 import Uncle from "../uncle/Uncle"
 import './Grandpa.css'
 
+const AssetContext = createContext("gold")
+
 const Grandpa = () => {
   return (
     <div className="grandpa">
         <h2>Grandpa</h2>
-       <section className="flex">
-       <Dad></Dad>
-        <Uncle></Uncle>
-        <Anty></Anty>
-       </section>
+        <AssetContext.Provider value="gold">
+            <section className="flex">
+                <Dad></Dad>
+                 <Uncle></Uncle>
+                 <Anty></Anty>
+             </section>
+        </AssetContext.Provider>
     </div>
   )
 }
