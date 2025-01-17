d869f6bdd97c0b53d20d76e3654d82a8d3550c59
Saayemm <sayemdcc@gmail.com>
2025-01-17 10:58:45 +0600

    error

--- src/components/stateFullForm/StateFullForm.jsx
+++ src/components/stateFullForm/StateFullForm.jsx
@@ -1,20 +1,49 @@
+import { useState } from "react"
 
 
 export default function StateFullForm() {
 
+    const [email, setEmail] = useState(null)
+    const [name, setName] = useState(null)
+    const [pass, setPass] = useState(null)
+    const [error, setError] = useState('')
+
     const hundleSubmit = e => {
+        e.preventDefault()
+        if(pass.length < 6 ){
+            setError("Password must be atleat 6 character long")
+        }else{
+            setError('')
+            console.log(email, name, pass)
+        }
         
     }
+
+    const hundleEmailChange = e => {
+        setEmail(e.target.value)
+    }
+
+    const hundlePassChange = e  => {
+        setPass(e.target.value)
+    }
+
+    const hundleNameChange = e  => {
+        setName(e.target.value)
+    }
+
   return (
     <div>
         <form onSubmit={hundleSubmit}>
-            <input  type="text" name="name" />
+            <input onChange={hundleNameChange}  type="text" name="name" />
             <br />
-            <input  type="email" name="email" />
+            <input onChange={hundleEmailChange}  type="email" name="email" />
             <br />
-            <input  type="password" name="password" />
+            <input onChange={hundlePassChange} type="password" name="password" />
             <br />
             <input type="submit" value="Submit"/>
+            {
+                error && <p>{error}</p>
+            }
         </form>
     </div>
   )
